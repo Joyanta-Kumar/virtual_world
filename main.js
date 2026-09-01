176ba56f0dc0835fb112248a1feb1181935b855d
Joyanta Kumar <joyroy122004@gmail.com>
2026-09-01 23:11:09 +0600

    cleared for a new start

--- main.js
+++
@@ -1,8 +0,0 @@
-const canvas = document.getElementById("canvas");
-
-canvas.width = window.innerWidth / 1.2
-canvas.height = window.innerHeight / 1.2
-
-canvas.addEventListener("mousemove", (event) => {
-    console.log(event.offsetX + ", " + event.offsetY)
-});
